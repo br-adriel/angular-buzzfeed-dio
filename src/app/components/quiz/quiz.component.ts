8617d8776389506bf3030b63ee33edc460510436
Adriel <adriel.fsantos@outlook.com>
2023-05-08 17:04:01 -0300

    Cria propriedades lógicas do componente quiz

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-quiz',
   templateUrl: './quiz.component.html',
-  styleUrls: ['./quiz.component.css']
+  styleUrls: ['./quiz.component.css'],
 })
 export class QuizComponent {
+  public title: string = '';
 
+  public questions: any;
+  public selectedQuestion: any;
+
+  public answers: string[] = [];
+  public selectedAnswer: string = '';
+
+  private questionIndex: number = 0;
+  private questionMaxIndex: number = 0;
+
+  public finished: boolean = false;
 }
