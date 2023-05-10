c38c74a2e74a8e9696a5aeec4acac6ddbfa10e09
Adriel <adriel.fsantos@outlook.com>
2023-05-09 21:27:49 -0300

    Implementa avanço de questões

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import quizQuestions from '../../../assets/data/quiz_questions.json';
 
 @Component({
   selector: 'app-quiz',
   templateUrl: './quiz.component.html',
   styleUrls: ['./quiz.component.css'],
 })
-export class QuizComponent {
+export class QuizComponent implements OnInit {
   public title: string = '';
 
   public questions: any;
@@ -18,4 +19,32 @@ export class QuizComponent {
   private questionMaxIndex: number = 0;
 
   public finished: boolean = false;
+
+  public chooseOption(alias: string): void {
+    this.answers.push(alias);
+    this.nextRound();
+  }
+
+  public nextRound(): void {
+    this.questionIndex++;
+
+    if (this.questionIndex > this.questionMaxIndex) {
+      this.finished = true;
+    } else {
+      this.selectedQuestion = this.questions[this.questionIndex];
+    }
+  }
+
+  ngOnInit(): void {
+    if (quizQuestions) {
+      this.finished = false;
+      this.title = quizQuestions.title;
+      this.questions = quizQuestions.questions;
+
+      this.questionIndex = 0;
+      this.questionMaxIndex = this.questions.length - 1;
+
+      this.selectedQuestion = this.questions[this.questionIndex];
+    }
+  }
 }
